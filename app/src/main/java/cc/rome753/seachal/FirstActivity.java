3a96940276665e25a13ba8db2c1d6f69f565e88d
Seachal <seachalzhang@gmail.com>
2020-01-03 17:53:48 +0800

    这个项目突然就编译不通过，有时间再验证码，
    singtask 已经验证了， 但是 他的 newIntent 方法还没有去重写，看在什么场景下回调用。

---
+++ app/src/main/java/cc/rome753/seachal/FirstActivity.java
@@ -0,0 +1,17 @@
+package cc.rome753.seachal;
+
+import cc.rome753.demo.BaseActivity;
+
+/**
+ * *
+ * *
+ * Project_Name:ActivityTaskView
+ *
+ * @author zhangxc
+ * @date 2020-01-02 18:52
+ * *
+ */
+public class FirstActivity extends BaseActivity {
+
+
+}
---
+++ app/src/main/java/cc/rome753/seachal/ThirdActivity.java
@@ -0,0 +1,15 @@
+package cc.rome753.seachal;
+
+import cc.rome753.demo.BaseActivity;
+
+/**
+ * *
+ * *
+ * Project_Name:ActivityTaskView
+ *
+ * @author zhangxc
+ * @date 2020-01-02 18:53
+ * *
+ */
+public class ThirdActivity extends BaseActivity {
+}
